e79205c27f6e12a7473f2d3baa41e92cf07089d6
mohamedarabid <105579261+mohamedarabid@users.noreply.github.com>
2022-07-31 11:14:41 +0200

    delete application

--- resources/views/control_panel/settings/users/roles/basic/form.blade.php
+++
@@ -1,8 +0,0 @@
-@csrf
-<div class="mb-3 row">
-    <label for="name" class="col-md-3 col-form-label">الاسم</label>
-    <div class="col-md-9">
-        <input class="form-control" type="text" name="name" value="{{old('name',$role->name)}}" id="name" required>
-    </div>
-</div>
-<input type="hidden" name="id" value="{{ $role->id }}">
